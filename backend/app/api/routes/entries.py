33798a545bccfb132681eef417dba22ba689dc3e
Harsh04Gautam <harsh99gautam@gmail.com>
2026-04-16 14:18:43 -0400

    Added temp test

--- backend/app/api/routes/entries.py
+++ backend/app/api/routes/entries.py
@@ -41,20 +41,6 @@ def get_entries(session: SessionDep, current_user: CurrentUser, event_id: uuid.U
     return entries
 
 
-@router.get("/{event_id}", response_model=EntriesPublic)
-def get_entries(session: SessionDep, current_user: CurrentUser, event_id: uuid.UUID):
-    event = crud.get_event(session=session, event_id=event_id)
-    if not event:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Event not found")
-    if event.user_id != current_user.id:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN, detail="Event not found")
-
-    entries = crud.get_entries_by_event(session=session, event_id=event_id)
-    return entries
-
-
 class Status(str, Enum):
     COMPLETED = "completed"
     PENDING = "pending"
--- backend/tests/api/routes/test_entries.py
+++ backend/tests/api/routes/test_entries.py
@@ -26,7 +26,42 @@ def test_get_entries_by_event(client: TestClient, normal_user_token_headers: dic
     event, blocks = random_event()
     event_id = event.id
 
+    data = {
+        "data": {
+            blocks[0]: random_lower_string(),
+            blocks[1]: 20,
+            blocks[2]: True,
+        },
+        "due_date": str(datetime.now())
+    }
+
+    client.post(f"{settings.API_V1_STR}/entries/{event_id}",
+                headers=normal_user_token_headers, json=data)
+
     r = client.get(f"{settings.API_V1_STR}/entries/{event_id}",
                    headers=normal_user_token_headers)
 
     assert r.status_code == 200
+    assert r.json()["data"]
+    assert r.json()["count"] > 0
+
+
+def test_get_calendar(client: TestClient, normal_user_token_headers: dict[str, str], random_event):
+    event, blocks = random_event()
+    event_id = event.id
+
+    for _ in range(5):
+        data = {
+            "data": {
+                blocks[0]: random_lower_string(),
+                blocks[1]: 20,
+                blocks[2]: True,
+            },
+            "due_date": str(datetime.now())
+        }
+
+        client.post(f"{settings.API_V1_STR}/entries/{event_id}",
+                    headers=normal_user_token_headers, json=data)
+
+    r = client.get(f"{settings.API_V1_STR}/entries/calendar?start_date={datetime.now()}",
+                   headers=normal_user_token_headers, json=data)
